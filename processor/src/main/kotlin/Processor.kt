13d08cd3f57e3be38e93420f0d3b9b82c4b77a5a
Dmitry Suzdalev <dimsuz@gmail.com>
2018-02-16 01:01:48 +0200

    Find TypeElements for diff source and receiver classes

--- processor/src/main/kotlin/Processor.kt
+++ processor/src/main/kotlin/Processor.kt
@@ -1,19 +1,27 @@
 package com.dimsuz.diffdispatcher.processor
 
 import com.dimsuz.diffdispatcher.annotations.DiffElement
-import javax.annotation.processing.*
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.ProcessingEnvironment
+import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.ElementKind
 import javax.lang.model.element.TypeElement
-import javax.tools.Diagnostic
+import javax.lang.model.type.TypeMirror
+import javax.lang.model.util.Types
+
+private const val RECEIVER_PARAMETER_NAME = "diffReceiver"
 
 class Processor : AbstractProcessor() {
     private lateinit var logger: Logger
+    private lateinit var typeUtils: Types
 
     @Synchronized
     override fun init(processingEnv: ProcessingEnvironment) {
         super.init(processingEnv)
 
         logger = Logger(processingEnv.messager)
+        typeUtils = processingEnv.typeUtils
         logger.note("starting annotation processing")
     }
 
@@ -26,9 +34,59 @@ class Processor : AbstractProcessor() {
     }
 
     override fun process(annotations: Set<TypeElement>, roundEnv: RoundEnvironment): Boolean {
-        logger.note("processing $annotations")
+        for (element in roundEnv.getElementsAnnotatedWith(DiffElement::class.java)) {
+            if (element.kind != ElementKind.CLASS) {
+                logger.error("${DiffElement::class.java.simpleName} can only be applied to class")
+                return true
+            }
+
+            val targetElement = element as TypeElement
+            val receiverElement = getReceiverElement(targetElement) ?: return true // error should already be printed
+
+            checkHasHashCodeEquals(targetElement)
+//            targetElement.enclosedElements.forEach {
+//                logger.note(it.simpleName.toString() + ", " + it.kind)
+//            }
+        }
+
         return true
+    }
+
+    private fun getReceiverElement(targetElement: TypeElement): TypeElement? {
+        val annotation = targetElement.annotationMirrors
+            .find {
+                (it.annotationType.asElement() as TypeElement).qualifiedName.toString() == DiffElement::class.java.name
+            }
+        if (annotation == null) {
+            // must be enforced by compiler, how come?
+            logger.error("internal error, no target annotation")
+            return null
+        }
+        val receiverValue = annotation.elementValues.entries
+            .firstOrNull { (element, _) ->
+                element.simpleName.toString() == RECEIVER_PARAMETER_NAME
+            }
+            ?.value
+        if (receiverValue == null) {
+            // must be enforced by compiler, how come?
+            logger.error("internal error, annotation misses $RECEIVER_PARAMETER_NAME property")
+            return null
+        }
+        val receiverTypeMirror = receiverValue.value as TypeMirror
+        return typeUtils.asElement(receiverTypeMirror) as TypeElement
+    }
+
+    private fun checkHasHashCodeEquals(typeElement: TypeElement) {
+        if (!hasHashCodeEquals(typeElement)) {
+            logger.warning("class \"${typeElement.simpleName}\" does not override equals/hashCode, " +
+                "this will restrict diffing to reference only comparisons")
+        }
     }
 
 }
 
+private fun hasHashCodeEquals(typeElement: TypeElement): Boolean {
+    val enclosedElements = typeElement.enclosedElements
+    return enclosedElements.any { it.kind == ElementKind.METHOD && it.simpleName.toString() == "equals" }
+        && enclosedElements.any { it.kind == ElementKind.METHOD && it.simpleName.toString() == "hashCode" }
+}
