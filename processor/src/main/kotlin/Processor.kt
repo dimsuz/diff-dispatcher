bde3b921ad633fbcdc6c6544656c0b7ede642eac
Dmitry Suzdalev <dimsuz@gmail.com>
2018-02-17 19:19:58 +0200

    Comment out logging

--- processor/src/main/kotlin/Processor.kt
+++ processor/src/main/kotlin/Processor.kt
@@ -48,9 +48,9 @@ class Processor : AbstractProcessor() {
             val receiverElement = getReceiverElement(targetElement) ?: return true // error should already be printed
 
             checkHasHashCodeEquals(targetElement)
-            targetElement.enclosedElements.forEach {
-                logger.note(it.simpleName.toString() + ", " + it.kind)
-            }
+//            targetElement.enclosedElements.forEach {
+//                logger.note(it.simpleName.toString() + ", " + it.kind)
+//            }
 
             val targetFields = targetElement.enclosedFields.map { TargetField(it) }
             // receiver interface method parameters grouped by method they belong to
